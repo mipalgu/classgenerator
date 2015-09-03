02ec6f57936ae61eca823949eb93d83415a19357
Mick Hawkins <m.hawkins@griffith.edu.au>
2015-09-03 17:26:41 +1000

    Added Tests target

--- classgenerator/myButton.h
+++ classgenerator/myButton.h
@@ -1,7 +1,7 @@
 /** 
  * file myButton.h 
  * 
- * Created by mick on Sat Aug 29 18:16:14 2015
+ * Created by mick on Sun Aug 30 18:32:57 2015
  * Copyright (c) 2015 mick 
  * All rights reserved. 
  * 
