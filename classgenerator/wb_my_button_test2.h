d5caf024d31dee8ae9f86b1d84f454b5db0707ac
Rene Hexel <r.hexel@griffith.edu.au>
2015-09-15 09:19:43 +1000

    Add Makefile for command line builds

--- classgenerator/wb_my_button_test2.h
+++ classgenerator/wb_my_button_test2.h
@@ -1,8 +1,8 @@
 /** 
  * file wb_my_button_test2.h 
  * 
- * Created by mick on Mon Sep 14 20:06:11 2015
- * Copyright (c) 2015 mick 
+ * Created by rh on Tue Sep 15 09:17:25 2015
+ * Copyright (c) 2015 rh 
  * 
  * This file was generated from my_button_test2.txt 
  * 
@@ -22,7 +22,7 @@
  * 3. All advertising materials mentioning features or use of this 
  *    software must display the following acknowledgement: 
  * 
- *    This product includes software developed by mick. 
+ *    This product includes software developed by rh. 
  * 
  * 4. Neither the name of the author nor the names of contributors 
  *    may be used to endorse or promote products derived from this 
