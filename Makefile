ad46bc9e9a5dabf7735fb2a7338ea06e89a0ddca
Mick Hawkins <m.hawkins@griffith.edu.au>
2015-09-19 11:14:22 +1000

    Sting manipulation class, moved methods from ClassData

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 #
 ALL_TARGETS=host xc
 
-SWIFT_SRCS=main.swift ClassData.swift fileio.swift GNUlicense.swift
+SWIFT_SRCS=main.swift ClassData.swift fileio.swift GNUlicense.swift stringmanipulation.swift
 
 USE_TWO_LEVEL_SRCDIR=yes
 
--- classgenerator/ClassData.swift
+++ classgenerator/ClassData.swift
@@ -29,6 +29,7 @@ class ClassData {
         // make wb_ name : the name not including .txt, with wb_ added
         self.wb = "wb_" + nameWithoutExtension[0]
         
+        // split the name into words, delimited by underscore
         let words = nameWithoutExtension[0].characters.split {$0 == "_"}.map { String($0) }
         
         self.camel = camelCaseWord(words)
@@ -68,73 +69,5 @@ func getPath() -> String {
     //print("Working directory: \(String.fromCString(path)!)")
     
     return String.fromCString(path)! + "/"
-}
-
-
-
-func upperCase (ch: Character) -> Character {
-    
-    if ( ch >= "a" ) && ( ch <= "z" ){
-        
-        let scalars = String(ch).unicodeScalars      // unicode scalar(s) of the character
-        let val = scalars[scalars.startIndex].value  // value of the unicode scalar
-        
-        return Character(UnicodeScalar(val - 32))    // return the capital
-    }
-    else {
-        
-        return ch                                    // return the character since it's not a letter
-    }
-}
-
-
-func capitalisedWord (word: String) -> String {
-    
-    var capWord = ""
-    var firstLetter = true
-    
-    for ch in word.characters {
-        
-        if firstLetter {
-            capWord += String(upperCase(ch))          // return an uppercase character as a String
-            firstLetter = false
-        }
-        else {
-            capWord += String(ch)
-        }
-    }
-
-    return capWord
-}
-
-
-func uppercaseWord (word: String) -> String {
-    
-    var uppWord = ""
-    
-    for ch in word.characters {
-        
-            uppWord += String(upperCase(ch))
-    }
-    
-    return uppWord
-}
-
-
-func camelCaseWord(words: [String]) -> String {
-    
-    var camelCase: String = ""
-    var wordToAdd = words[0]
-    
-    for word in words {
-        if camelCase.characters.count > 0 {
-            
-            wordToAdd = capitalisedWord(word)
-        }
-        
-        camelCase += wordToAdd
-    }
-    
-    return camelCase
 }
 
