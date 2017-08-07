50d95995aba7a33da6e609485323f086d397a8ff
Callum McColl <callum.mccoll1@gmail.com>
2017-08-07 14:33:51 +1000

    Created SectionsParserTests.swift

--- new/Tests/LinuxMain.swift
+++ new/Tests/LinuxMain.swift
@@ -2,5 +2,7 @@ import XCTest
 @testable import classgeneratorTests
 
 XCTMain([
-    testCase(ClassGeneratorTests.allTests)
+    testCase(ClassGeneratorTests.allTests),
+    testCase(Parser.allTests),
+    testCase(SectionsParser.allTests)
 ])
