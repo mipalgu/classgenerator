bc1e87b68a0a6815da2752f40cc018d10f365a73
Callum McColl <callum.mccoll1@gmail.com>
2018-01-05 13:46:57 +1000

    Added whiteboard_helpers dependency in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "classgenerator",
+    dependencies: [
+        .package(url: "ssh://git.mipal.net/git/whiteboard_helpers.git", .branch("master"))
+    ],
     targets: [
         .target(name: "classgenerator", dependencies: []),
         .testTarget(name: "classgeneratorTests", dependencies: [.target(name: "classgenerator")])
