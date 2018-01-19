0889d5a091bbf70ec7138b393a1f205faf772fa5
Callum McColl <callum.mccoll1@gmail.com>
2018-01-19 15:50:33 +1000

    Got from_string working

--- temp/main.cc
+++ temp/main.cc
@@ -6,11 +6,20 @@
 #define WHITEBOARD_POSTER_STRING_CONVERSION 1
 
 #include "TopParticles.h"
+#include "ParticlePosition.h"
+
+using namespace guWhiteboard;
 
 int main() {
-    guWhiteboard::TopParticles p = guWhiteboard::TopParticles();
+    TopParticles p = TopParticles();
     std::cout << p.description() << std::endl;
     std::cout << p.to_string() << std::endl;
+    ParticlePosition ps[4] = {ParticlePosition(wb_point2D(), 1), ParticlePosition(wb_point2D(), 2), ParticlePosition(wb_point2D(), 3), ParticlePosition(wb_point2D(), 4)};
+    p.set_particles(ps[0], 0);
+    p.set_particles(ps[1], 1);
+    p.set_particles(ps[2], 2);
+    p.set_particles(ps[3], 3);
+    std::cout << p.description() << std::endl;
     return EXIT_SUCCESS;
 }
 
