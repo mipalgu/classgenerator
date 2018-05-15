d9d0f718f7ad4568fc5782770b6ab6f9c51b0d5d
Callum McColl <callum.mccoll1@gmail.com>
2018-05-15 11:29:14 +1000

    Made enumerated types associate a name of the type

--- Sources/Data/VariableTypes.swift
+++ Sources/Data/VariableTypes.swift
@@ -62,7 +62,7 @@ public enum VariableTypes {
     case bit
     case bool
     case char(CharSigns)
-    case enumerated
+    case enumerated(String)
     case gen(String, String, String) // gen name, struct name, class name
     case numeric(NumericTypes)
     indirect case pointer(VariableTypes)
