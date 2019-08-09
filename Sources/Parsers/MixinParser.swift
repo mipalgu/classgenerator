92af6207949dd033713509b4495cbee605df4b1f
Callum McColl <callum.mccoll1@gmail.com>
2019-08-09 12:27:24 +1000

    Implemented mixin parser so that it can parse both declarations and calls

--- Sources/Parsers/MixinParser.swift
+++ Sources/Parsers/MixinParser.swift
@@ -65,14 +65,10 @@ public final class MixinParser {
         let trimmedLine = line.trimmingCharacters(in: .whitespaces)
         let marker = "@include"
         if false == trimmedLine.hasPrefix(marker) {
-            throw ParsingErrors.parsingError(0, "The mixin line must start with @include")
+            throw ParsingErrors.parsingError(0, "The mixin line must start with \(marker)")
         }
         let trimmed = trimmedLine.dropFirst(marker.count).trimmingCharacters(in: .whitespaces)
         let split = trimmed.components(separatedBy: "(")
-        if split.count > 2 {
-            let index = line.firstIndex(of: "(").map { line.distance(from: line.startIndex, to: $0) } ?? 0
-            throw ParsingErrors.parsingError(index, "You can only specify one variable list")
-        }
         let filePathStr = split[0].trimmingCharacters(in: .whitespaces)
         if filePathStr.isEmpty {
             throw ParsingErrors.parsingError(0, "The file path cannot be empty.")
@@ -85,39 +81,60 @@ public final class MixinParser {
         } else {
             throw ParsingErrors.parsingError(0, "Malformed mixin name.")
         }
+        let variables = try self.parseVariablesList(line: trimmedLine, delimiter: ":", allowEmpty: false)
+        return (filePath, Dictionary(uniqueKeysWithValues: variables.map { ($0, $1!) }))
+    }
+
+    public func parseDeclaration(line: String) throws -> [String: String?] {
+        let trimmedLine = line.trimmingCharacters(in: .whitespaces)
+        let marker = "@mixin"
+        if false == trimmedLine.hasPrefix(marker) {
+            throw ParsingErrors.parsingError(0, "The mixin line must start with \(marker)")
+        }
+        return try self.parseVariablesList(line: trimmedLine, delimiter: "=", allowEmpty: true)
+    }
+
+    fileprivate func parseVariablesList(line: String, delimiter: String, allowEmpty: Bool) throws -> [String: String?] {
+        let split = line.trimmingCharacters(in: .whitespaces).components(separatedBy: "(")
+        if split.count > 2 {
+            let index = line.firstIndex(of: "(").map { line.distance(from: line.startIndex, to: $0) } ?? 0
+            throw ParsingErrors.parsingError(index, "You can only specify one variable list")
+        }
         if split.count < 2 {
-            return (filePath, [:])
+            return [:]
         }
         let varList = split[1].trimmingCharacters(in: .whitespaces).components(separatedBy: ")")[0]
         if varList.isEmpty {
-            return (filePath, [:])
+            return [:]
         }
         let vars: [String] = varList.components(separatedBy: ",").map { $0.trimmingCharacters(in: .whitespaces) }
         var nameSet: Set<String> = []
         nameSet.reserveCapacity(vars.count)
-        guard let allVars: [(String, String)] = try vars.failMap({ (str: String) throws -> (String, String)? in
-            let components = str.components(separatedBy: ":")
-            if components.count != 2 {
-                throw ParsingErrors.parsingError(0, "Malformed parameter list in mixin call.")
-            }
+        guard let allVars: [(String, String?)] = try vars.failMap({ (str: String) throws -> (String, String?)? in
+            let components = str.components(separatedBy: delimiter)
             let name = components[0].trimmingCharacters(in: .whitespaces)
-            let value = components[1].trimmingCharacters(in: .whitespaces)
-            if name.isEmpty || value.isEmpty {
-                throw ParsingErrors.parsingError(0, "Malformed parameter list in mixin call.")
+            if name.isEmpty {
+                throw ParsingErrors.parsingError(0, "Malformed parameter list in mixin.")
             }
             if nameSet.contains(name) {
-                throw ParsingErrors.parsingError(0, "Duplicate variable in parameter list for mixin call.")
+                throw ParsingErrors.parsingError(0, "Duplicate variable in parameter list for mixin.")
             }
             nameSet.insert(name)
+            if components.count < 2 && allowEmpty {
+                return (name, nil)
+            }
+            if components.count != 2 {
+                throw ParsingErrors.parsingError(0, "Malformed parameter list in mixin.")
+            }
+            let value = components[1].trimmingCharacters(in: .whitespaces)
+            if value.isEmpty {
+                throw ParsingErrors.parsingError(0, "Malformed parameter list in mixin.")
+            }
             return (name, value)
         }) else {
             throw ParsingErrors.parsingError(0, "Unable to parse \(line).")
         }
-        return (filePath, Dictionary<String, String>(uniqueKeysWithValues: allVars))
-    }
-
-    public func parseDeclaration(line: String) throws -> [String: String?] {
-        throw ParsingErrors.parsingError(0, "Not yet implemented")
+        return Dictionary<String, String?>(uniqueKeysWithValues: allVars)
     }
 
 }
